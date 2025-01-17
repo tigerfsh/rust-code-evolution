736c4242dfad5b2701e1b14023781daee3c5e60d
fushouhai <fushouhai@starmerx.com>
2025-01-17 16:11:23 +0800

    add some comment

--- tests/as_ref_test.rs
+++ tests/as_ref_test.rs
@@ -5,6 +5,12 @@ fn calculate_length() {
     let a = String::from("hello,world");
     let b = "hello,world";
 
+    /*
+    https://doc.rust-lang.org/std/convert/trait.AsRef.html#reflexivity
+    这段代码的意思是：如果 T 实现了 AsRef<str>，那么 &T 也会自动实现 AsRef<str>。
+    实现的方式是通过解引用 self（即 *self），然后调用 T 的 as_ref() 方法。
+    */
+
     calculate_length_v3(&a);
     calculate_length_v3(a);
     calculate_length_v3(b);
